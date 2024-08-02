825d72effedfbfce9206bc3a0f0d23992b9f662a
Zachary Morehouse <u7637337@anu.edu.au>
2024-08-03 09:31:08 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,17 +6,38 @@ function setup() {
   // this isn't compulsory (remove this code if you like)
   strokeWeight(5);
   rect(0, 0, width, height);
+  strokeWeight(1);
+
 }
 
 function draw() {
-  // your cool monster code goes in this draw function
   
+  eyeball(150, 275, false)
+  eyeball(400, 200, false)
+  eyeball(650, 275, false)
+
+
 }
 
-// when you hit the spacebar, what's currently on the canvas will be saved (as a
-// "monster.png" file) to your downloads folder
+
 function keyTyped() {
   if (key === " ") {
     saveCanvas("monster.png");
   }
+}
+
+
+function eyeball(x, y, angry) {
+  fill(255);
+  circle(x,y, 100);
+  fill(0);
+  circle(x,y, 25)
+  if (angry === true) {
+      fill('green');
+    arc(x,y,100,100,PI, 0)
+  }
+
+  fill('green');
+  arc(x,y+35,75,30,0, PI)
+
 }
