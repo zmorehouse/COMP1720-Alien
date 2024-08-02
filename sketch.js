9a74b2ec5fd26bca2921e5ca53e5e99de00f0049
Zachary Morehouse <u7637337@anu.edu.au>
2024-08-03 09:56:42 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,15 +8,27 @@ function setup() {
   rect(0, 0, width, height);
   strokeWeight(1);
 
+
+
 }
 
+let angry = false;
+
 function draw() {
+background(255)
+
+  if (frameCount % 300 === 0) {
+     angry = !angry;
+  }
   
-  eyeball(150, 275, false)
-  eyeball(400, 200, false)
-  eyeball(650, 275, false)
-
-
+  strokeWeight(1);
+  eyeball(150, 275, angry)
+  eyeball(400, 200, angry)
+  eyeball(650, 275, angry)
+  
+  strokeWeight(0);
+  circle(width/2, 700, 500)
+  rect(151, 700, 498, 400, 20);
 }
 
 
@@ -26,18 +38,27 @@ function keyTyped() {
   }
 }
 
-
 function eyeball(x, y, angry) {
   fill(255);
   circle(x,y, 100);
   fill(0);
   circle(x,y, 25)
+
   if (angry === true) {
       fill('green');
-    arc(x,y,100,100,PI, 0)
+    arc(x,y,100,100,PI, 0);
+    fill('black');
+    strokeWeight(5);
+    line(x - 35, y-45, x, y - 35 )
+    line(x + 35, y-45, x, y - 35 )
+        strokeWeight(1);
   }
 
   fill('green');
   arc(x,y+35,75,30,0, PI)
 
+}
+
+function mouth(angry) {
+  
 }
